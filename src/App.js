3d98bcdc0a9e94141db13090180d7e1a24f2c348
guimariz <guimariz@gmail.com>
2021-02-05 19:02:06 -0300

    get com axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import * as api from './api/apiService';
 
 export default function App() {
+  const testApi = async () => {
+    const result = await api.getAllGrades();
+    console.log(result);
+  };
+
+  testApi();
   return <div></div>;
 }
